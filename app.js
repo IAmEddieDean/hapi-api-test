b26ecbce4dc1187150a8826278a2300ce4b99f16
Chris Oyler <christopher.oyler@gmail.com>
2016-01-06 22:50:55 -0800

    adds iife to app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-'use strict';
-
-var Hoek = require('hoek');
-var Server = require('./lib/server');
-var db = require('./db');
-
-Server.init(function(err, server){
-  Hoek.assert(!err, err);
-  console.log('--------------------------------------------------------------------------------');
-  console.log('Hapi:\n', server.info.uri);
-  console.log('Environment:\n', server.app.environment);
-  console.log('--------------------------------------------------------------------------------');
-});
+(function(){
+  'use strict';
+  
+  var Hoek = require('hoek');
+  var Server = require('./lib/server');
+  var db = require('./db');
+  
+  Server.init(function(err, server){
+    Hoek.assert(!err, err);
+    console.log('--------------------------------------------------------------------------------');
+    console.log('Hapi:\n', server.info.uri);
+    console.log('Environment:\n', server.app.environment);
+    console.log('--------------------------------------------------------------------------------');
+  });
+})();
